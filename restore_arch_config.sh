27c4d99542304eb897c80e4b14087e6d97aa3d64
cjacquem <cjacquem@student.42.fr>
2017-08-24 12:41:07 +0200

    Update

--- restore_arch_config.sh
+++ restore_arch_config.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sudo pacman -Rsc firefox hexchat || \
-sudo pacman -Syu && \
-sudo pacman -S git terminator vlc aria2 chromium bleachbit vim transmission \
-lldb valgrind zsh
+    sudo pacman -Syu && \
+    sudo pacman -S git terminator vlc aria2 chromium bleachbit vim transmission \
+    lldb valgrind zsh
 
 #Git configuration
 git config --global user.name "cjacquem"
@@ -18,29 +18,35 @@ then
 fi
 
 #Creation du dossier projects
-if [ ! -d "~/Documents/projects" ]; then
-	mkdir ~/Documents/projects
-	cd ~/Documents/projects
+if [ ! -d "~/Documents/projects" ]
+then
+    echo "Create projects directory"
+    mkdir ~/Documents/projects 
 fi
 #Creation du dossier 42
-if [ ! -d "~/Documents/projects/42" ]; then
-	mkdir ~/Documents/projects/42 && cd ~/Documents/projects/42
-        if [ ! -d "./libft/" ]; then
-            git clone git@github.com:kalak-io/libft.git
-        fi
-        if [ ! -d "./ft_printf/" ]; then
-            git clone git@github.com:kalak-io/ft_printf.git
-        fi
+if [ ! -d "~/Documents/projects/42" ]
+then 
+    echo "Create 42 projects directory"
+    mkdir ~/Documents/projects/42 && \
+        cd ~/Documents/projects/42
+fi
+if [ ! -d "~/Documents/projects/42/libft/" ]
+then
+    git clone git@github.com:kalak-io/libft.git
+fi
+if [ ! -d "~/Documents/projects/42/ft_printf/" ]
+then
+    git clone git@github.com:kalak-io/ft_printf.git
 fi
 #Creation du dossier personal
 if [ ! -d "~/Documents/projects/personal" ]; then
-	mkdir ~/Documents/projects/personal
-        cd ~/Documents/projects/personal
-        git clone git@github.com:kalak-io/configuration.git
-        git clone git@github.com:kalak-io/menu_generator.git
-        git clone git@gitlab.com:kalak/pepper_dialog_generator.git
-        git clone git@gitlab.com:kalak/pepper_conjugation_generator.git
-        git clone git@gitlab.com:kalak/pepper_complete_lexicon.git
+    mkdir ~/Documents/projects/personal
+    cd ~/Documents/projects/personal
+    git clone git@github.com:kalak-io/configuration.git
+    git clone git@github.com:kalak-io/menu_generator.git
+    git clone git@gitlab.com:kalak/pepper_dialog_generator.git
+    git clone git@gitlab.com:kalak/pepper_conjugation_generator.git
+    git clone git@gitlab.com:kalak/pepper_complete_lexicon.git
 fi
 
 #Configuration Vim
@@ -48,17 +54,17 @@ rm -rf ~/.vimrc ~/.vim
 ln -s ~/Documents/projects/personal/configuration/vimrc ~/.vimrc
 mkdir ~/.vim && mkdir ~/.vim/bundle
 git https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim && \
-vim +PluginInstall +qall
+    vim +PluginInstall +qall
 
 #Configuration gitignore_global
 ln -s ~/Documents/projects/personal/configuration/gitignore_global \
-~/.gitignore_global && git config --global core.excludesfile ~/.gitignore_global
+    ~/.gitignore_global && git config --global core.excludesfile ~/.gitignore_global
 
 
 #Creation du dossier professional
 if [ ! -d "~/Documents/projects/professional" ]; then
-	mkdir ~/Documents/projects/professionnal
-        cd ~/Documents/projects/professionnal
+    mkdir ~/Documents/projects/professionnal
+    cd ~/Documents/projects/professionnal
 fi
 cd
 #Configuration zsh

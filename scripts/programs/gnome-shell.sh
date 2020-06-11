699c73c9dd7b4ab21d82e2d878210059d106906d
Clément Jacquemaire <clement@unifai.fr>
2020-06-11 11:41:31 +0200

    Add settings in gnome-shell

--- scripts/programs/gnome-shell.sh
+++ scripts/programs/gnome-shell.sh
@@ -1,3 +1,6 @@
 #! /usr/bin/env bash
 
-echo "No Gnome shell extensions"
+echo "Configuring Gnome shell"
+gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize'
+gsettings set org.gnome.desktop.interface show-battery-percentage true
+gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'

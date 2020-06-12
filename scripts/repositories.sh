6ca477046005b32eb4a737ae9298e7bacfebeae3
Clément Jacquemaire <clement@unifai.fr>
2020-06-12 17:04:05 +0200

    Fix variables usage and Update key generation

--- scripts/repositories.sh
+++ scripts/repositories.sh
@@ -14,26 +14,28 @@ UNIFAI="
 
 echo "Pulling git repositories"
 # Create ssh key
-if [ -e "$HOME/.ssh/id_rsa" ]; then
-    ssh-keygen -t rsa -b 4096 -C "clement@unifai.fr"
+SSH_KEY=$HOME/.ssh/id_rsa
+if [ ! -f "$SSH_KEY" ]; then
+    echo "Generating ssh key"
+    ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa 2>/dev/null <<< y >/dev/null -b 4096 -C "clement@unifai.fr"
     eval "$(ssh-agent -s)" && ssh-add ~/.ssh/id_rsa
     xclip -sel clip < $HOME/.ssh/id_rsa.pub
     echo "Contents of the id_rsa.pub copied!"
-fi
-read -rp 'Did you add your ssh keys in different git services ? Y or N ' answer
-if [ "$answer" = 'N' ] || [ "$answer" = 'n' ]; then
-    exit 1
+    read -rp 'Did you add your ssh keys in different git services ? Y or N ' answer
+    if [ "$answer" = 'N' ] || [ "$answer" = 'n' ]; then
+        exit 1
+    fi
 fi
 
 # Personal repositories
 mkdir -p $HOME/Documents/projects/personal && cd $_
-for p in PERSONAL; do 
+for p in $PERSONAL; do
     git clone $p;
 done
 
 # UNiFAi repositories
 mkdir -p $HOME/Documents/projects/profesionnal/unifai && cd $_
-for u in UNIFAI; do 
+for u in $UNIFAI; do
     git clone $u;
 done
 

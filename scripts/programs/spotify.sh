5708fecdacc541d5d431d4799185cfdd094414c5
Clément Jacquemaire <clement@unifai.fr>
2020-06-12 16:24:26 +0200

    Update spotify with scale solution

--- scripts/programs/spotify.sh
+++ scripts/programs/spotify.sh
@@ -5,3 +5,4 @@ wget -O- https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add -
 sudo add-apt-repository "deb http://repository.spotify.com stable non-free"
 
 sudo apt-fast install -y spotify-client
+./$HOME/Documents/projects/personal/setup_fresh_installation_os/scripts/scale_spotify.sh
---
+++ scripts/scale_spotify.sh
@@ -0,0 +1,4 @@
+#! /usr/bin/env bash
+
+sudo rm /usr/share/applications/spotify.desktop
+sudo ln -s $HOME/Documents/projects/personal/setup_fresh_installation_os/spotify.desktop /usr/share/applications/spotify.desktop

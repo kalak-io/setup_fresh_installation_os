d93d0eb8a2fb0184c46dd224082f77e8a9ed8c3c
Clément Jacquemaire <clement@unifai.fr>
2020-08-19 11:24:28 +0200

    ADD youcompleteme installation

--- scripts/programs/vim.sh
+++ scripts/programs/vim.sh
@@ -3,4 +3,4 @@
 echo "Configuring vim"
 mkdir -p $HOME/.vim/bundle/
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-vim +PluginInstall +qall
+vim +PluginInstall +qall && python $HOME/.vim/bundle/youcompleteme/install.py --ts-completer

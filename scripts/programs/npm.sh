6dc9dfcac61aad44fc843a2175e37eaaf4ecce57
Clément Jacquemaire <clement@unifai.fr>
2020-08-24 16:50:58 +0200

    :wrench: UPDATE installation npm global modules

---
+++ scripts/programs/npm.sh
@@ -0,0 +1,17 @@
+#! /usr/bin/env bash
+
+echo "Installing Node & npm"
+sudo apt-fast install -y nodejs npm
+npm config set prefix $HOME/.npm  
+sudo chown -R $USER $HOME/.npm
+
+echo "Installing Vue"
+npm install -g vue
+npm install -g @vue/cli
+npm install -g nuxt
+
+echo "Installing Gitmoji"
+npm install -g gitmoji-cli
+
+echo "Installing linting"
+npm install -g eslint prettier eslint-plugin-vue 
--- scripts/programs/vuejs.sh
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env bash
-
-echo "Installing Vue.js"
-sudo apt-fast install -y nodejs npm
-npm config set prefix $HOME/.npm  
-sudo chown -R $USER $HOME/.npm
-npm install -g vue
-npm install -g @vue/cli

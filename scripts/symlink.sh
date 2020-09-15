c4297804e6fc5805d6b902cd043322d8bba90f66
Clément Jacquemaire <clement@unifai.fr>
2020-09-15 18:23:10 +0200

    :bug: FIX rm with folder

--- scripts/symlink.sh
+++ scripts/symlink.sh
@@ -12,7 +12,7 @@ git config --global core.excludesfile $HOME/.gitignore_global
 rm -f $HOME/.gitmessage
 ln -s $HOME/Documents/projects/personal/setup_fresh_installation_os/gitmessage $HOME/.gitmessage
 # .zshrc
-rm -f $HOME/.oh-my-zsh
+rm -rf $HOME/.oh-my-zsh
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 rm -f $HOME/.zshrc $HOME/.zshrc.pre-oh-my-zsh
 ln -s $HOME/Documents/projects/personal/setup_fresh_installation_os/zshrc $HOME/.zshrc

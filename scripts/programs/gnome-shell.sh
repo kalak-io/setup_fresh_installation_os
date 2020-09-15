b5e39117620db5b7774f975bfdde5f73ff9502c5
Clément Jacquemaire <clement@unifai.fr>
2020-09-15 18:30:52 +0200

    :fire: REMOVE codium installation

--- scripts/programs/gnome-shell.sh
+++ scripts/programs/gnome-shell.sh
@@ -5,4 +5,4 @@ gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize'
 gsettings set org.gnome.desktop.interface show-battery-percentage true
 gsettings set org.gnome.desktop.interface gtk-theme 'Yaru-dark'
 gsettings set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM
-gsettings set org.gnome.shell favorite-apps ['org.gnome.Nautilus.desktop', 'chromium_chromium.desktop', 'firefox.desktop', 'firefoxDeveloperEdition.desktop', 'codium.desktop', 'slack.desktop', 'spotify.desktop']
+gsettings set org.gnome.shell favorite-apps ['org.gnome.Nautilus.desktop', 'chromium_chromium.desktop', 'firefox.desktop', 'firefoxDeveloperEdition.desktop', 'slack.desktop', 'spotify.desktop']

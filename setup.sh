614198f9eaf1707fd32d9d90790afb53964236e4
Clément Jacquemaire <clement@unifai.fr>
2020-09-17 13:26:30 +0200

    :sparkles: UPDATE with conditional activation

--- setup.sh
+++ setup.sh
@@ -1,18 +1,46 @@
 #! /usr/bin/env bash
 
-# Dependencies
-sudo apt update && sudo apt install -y git curl vim zsh
+DISTRO=`lsb_release -is`
+# Requirements
+if [ $DISTRO == "Linuxmint" ] || [ $DISTRO == "Ubuntu" ]
+then
+    echo "Ubuntu base";
+    sudo apt update && sudo apt install -y git
+elif [ $DISTRO == "Manjaro" ]
+then
+    echo "Manjaro";
+    sudo pacman -Syy && yes | sudo pacman -S git
+else
+    echo "Unsupported distro";
+    exit 2
+fi
 
 # Setup personal filesystem
 echo "Create projects directory"
 mkdir -p $HOME/Documents/projects/personal && cd $_
 git clone https://github.com/kalak-io/setup_fresh_installation_os.git
 cd $HOME/Documents/projects/personal/setup_fresh_installation_os/scripts
-
 sudo chmod u+x ./*.sh ./programs/*.sh
 
+# Install specific packages
+if [ $DISTRO == "Linuxmint" ] || [ $DISTRO == "Ubuntu" ]
+then
+    echo "Ubuntu base";
+    ./ubuntu.sh
+elif [ $DISTRO == "Manjaro" ]
+then
+    echo "Manjaro";
+    ./manjaro.sh
+else
+    echo "Unsupported distro";
+fi
+
+# Install common packages
+for f in ./programs/common/*.sh; do
+    bash "$f" -H;
+done
+
 ./symlink.sh
-./ubuntu.sh
 ./repositories.sh
 
 echo "Finish ! You can reboot the system."

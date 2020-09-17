fb7908c922fe7edcdcd53eea590399765ba8297a
Clément Jacquemaire <clement@unifai.fr>
2020-09-17 23:49:00 +0200

    :bug: UPDATE chmod for all scripts in different folders

--- setup.sh
+++ setup.sh
@@ -20,7 +20,7 @@ echo "Create projects directory"
 mkdir -p $HOME/Documents/projects/personal && cd $_
 git clone https://github.com/kalak-io/setup_fresh_installation_os.git
 cd $HOME/Documents/projects/personal/setup_fresh_installation_os/scripts
-sudo chmod u+x ./*.sh ./programs/*.sh
+sudo chmod -R u+x ./*.sh ./**/*.sh
 
 # Install specific packages
 if [ $DISTRO == "Ubuntu" ]

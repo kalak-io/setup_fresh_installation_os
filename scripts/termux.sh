ecf907b60f64077a45522f450c080157631ca28d
kalak <clement@unifai.fr>
2020-09-18 17:07:41 +0200

    ADD termux sceipt

---
+++ scripts/termux.sh
@@ -0,0 +1,19 @@
+#! /usr/bin/env bash
+
+PACKAGES=(
+    python
+    vim
+    zsh
+    aria2
+)
+
+# Update packages
+echo "Updating packages"
+pkg update && pkg upgrade -y
+
+# Enable storage
+echo "Enable access storage"
+termux-setup-storage
+
+# Install packages
+pkg install -y ${PACKAGES[*]}

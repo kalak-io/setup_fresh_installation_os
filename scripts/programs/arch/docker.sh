35ef576f4866aa13d859dc48b46535ee4d60dbbb
Clément Jacquemaire <clement@unifai.fr>
2020-09-22 23:19:53 +0200

    :wrench: ADD script to install docker-compose in arch

---
+++ scripts/programs/arch/docker.sh
@@ -0,0 +1,6 @@
+#! /usr/bin/env bash
+
+# Install Docker Compose
+echo "Installing Docker Compose"
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose

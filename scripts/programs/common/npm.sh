5c21a1dae03c621bf43807055ca75b875c69b330
Clément Jacquemaire <clement@unifai.fr>
2020-10-30 11:49:20 +0100

    :heavy_plus_sign: ADD more global packages : bit-bin, json-server

--- scripts/programs/common/npm.sh
+++ scripts/programs/common/npm.sh
@@ -9,9 +9,13 @@ echo "Installing Vue"
 npm install -g vue
 npm install -g @vue/cli
 npm install -g nuxt
+npm install -g json-server
 
 echo "Installing Gitmoji"
 npm install -g gitmoji-cli
 
 echo "Installing linting"
 npm install -g eslint prettier eslint-plugin-vue 
+
+echo "Installing bit.dev"
+npm install -g bit-bin

fcf2967d26d3466aa9e08745795ca3c079d3a725
Clément Jacquemaire <clement@unifai.fr>
2020-12-01 00:05:58 +0100

    :heavy_plus_sign: ADD method to install Barista for i3status

--- scripts/de/i3.sh
+++ scripts/de/i3.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
+echo "Creating symlinks"
 ln -s $HOME/Documents/projects/personal/setup_fresh_installation_os/config/i3 $HOME/.config/i3
 ln -s $HOME/Documents/projects/personal/setup_fresh_installation_os/config/i3status $HOME/.config/i3status
+ln -s $HOME/Documents/projects/personal/setup_fresh_installation_os/config/dunst $HOME/.config/dunst
+
+echo "Installing Barista"
+curl -L https://git.io/fA7iT -o $HOME/Documents/projects/personal/setup_fresh_installation_os/config/barista
+sudo cmhod +x $HOME/Documents/projects/personal/setup_fresh_installation_os/config/barista

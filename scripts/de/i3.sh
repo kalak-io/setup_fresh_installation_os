d25c6a5038e0d4bc00f0b1085a5e943eed0164fe
Clément Jacquemaire <clement@unifai.fr>
2020-11-30 23:11:36 +0100

    :wrench: UPDATE with symlink to i3status config

--- scripts/de/i3.sh
+++ scripts/de/i3.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 ln -s $HOME/Documents/projects/personal/setup_fresh_installation_os/config/i3 $HOME/.config/i3
+ln -s $HOME/Documents/projects/personal/setup_fresh_installation_os/config/i3status $HOME/.config/i3status

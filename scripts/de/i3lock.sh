91c26dd49c0ef435454d4130b4015c66911bafdb
Clément Jacquemaire <clement@unifai.fr>
2020-12-07 17:57:19 +0100

    :lipstick: ADD custom image in lock

--- scripts/de/i3lock.sh
+++ scripts/de/i3lock.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
+ICON=$HOME/Pictures/avatar_kalak.png
 PICTURE=/tmp/i3lock.png
-SCREENSHOT="scrot $PICTURE"
+BLUR="0x6"
 
-BLUR="5x4"
-
-$SCREENSHOT
+scrot $PICTURE
 convert $PICTURE -blur $BLUR $PICTURE
+convert $PICTURE $ICON -gravity center -composite $PICTURE
 i3lock -i $PICTURE
 rm $PICTURE

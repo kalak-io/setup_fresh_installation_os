705665312b6bf309cc75bf8636ebbf12d4ae23f0
Clément Jacquemaire <clement@unifai.fr>
2020-12-10 23:44:39 +0100

    :rotating_light: REMOVE whitespace

--- scripts/programs/common/python.sh
+++ scripts/programs/common/python.sh
@@ -4,4 +4,4 @@ echo "🐍 Installing Python Packages"
 pip3 install pre-commit virtualenv virtualenvwrapper
 
 git clone https://github.com/pyenv/pyenv.git $HOME/.pyenv
-git clone https://github.com/yyuu/pyenv-virtualenv.git   $HOME/.pyenv/plugins/pyenv-virtualenv
+git clone https://github.com/yyuu/pyenv-virtualenv.git $HOME/.pyenv/plugins/pyenv-virtualenv

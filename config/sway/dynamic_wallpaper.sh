d1a5d1947f7af61b4d10cb29c1d6432ef4701960
Clément Jacquemaire <clement@unifai.fr>
2020-12-21 00:13:09 +0100

    :lipstick: ADD some images

--- config/sway/dynamic_wallpaper.sh
+++ config/sway/dynamic_wallpaper.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-swaybg -i ./img/$(date -u -d @$((($(date -u +%s)
+swaybg -i ~/.config/sway/img/$(date -u -d @$((($(date -u +%s)
                  / 600) * 600)) "+%Hh%M").png -m fill &
 OLD_PID=$!
 while true; do
     sleep 600
-    swaybg -i ./img/$(date -u -d @$((($(date -u +%s)
+    swaybg -i ~/.config/sway/img/$(date -u -d @$((($(date -u +%s)
                      / 600) * 600)) "+%Hh%M").png -m fill &
     NEXT_PID=$!
     sleep 5
